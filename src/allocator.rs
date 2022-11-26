6e29585eca171e3028cbee59bd126f3291ad6547
Finn Mattis <planto73@harkenpretty.com>
2022-11-25 22:56:45 -0500

    Added bump allocator and custom linked list allocator (does not merge unused memory)

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,6 +1,9 @@
 use alloc::alloc::{GlobalAlloc, Layout};
 use core::ptr::null_mut;
-use linked_list_allocator::LockedHeap;
+use linked_list::LinkedListAllocator;
+
+pub mod bump;
+pub mod linked_list;
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024; // 100 KiB
@@ -18,7 +21,7 @@ unsafe impl GlobalAlloc for Dummy {
 }
 
 #[global_allocator]
-static ALLOCATOR: LockedHeap = LockedHeap::empty();
+static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
 
 use x86_64::{
     structures::paging::{
@@ -52,4 +55,30 @@ pub fn init_heap(
     }
 
     Ok(())
+}
+
+pub struct Locked<A> {
+    inner: spin::Mutex<A>,
+}
+
+impl<A> Locked<A> {
+    pub const fn new(inner: A) -> Self {
+        Locked {
+            inner: spin::Mutex::new(inner),
+        }
+    }
+
+    pub fn lock(&self) -> spin::MutexGuard<A> {
+        self.inner.lock()
+    }
+}
+
+//align the addr upwards to alignment align - requires that `align` is a power of two.
+fn align_up(addr: usize, align: usize) -> usize {
+    //Align is a power of 2, so align - 1 is a bitmask of all lower bits
+    //e.g. align = 0b1000 -> align - 1 = 0b0111
+    //By using bitwise NOT we get a bitmask of all higher bits
+    //e.g. align = 0b0001 -> !align = 0b1110
+    //The bitwise AND aligns downward so we need to increase addr by align - 1 to align upward
+    (addr + align - 1) & !(align - 1)
 }
